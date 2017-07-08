954925c1cf1228610b0cb476e074ddacd7d33ab0
UniversePeace <liuyuning@liuyuningdeMacBook-Pro.local>
2017-07-08 22:55:33 +0800

    Database commit

---
+++ app/src/main/java/com/example/universepeace/coolweather/db/Province.java
@@ -0,0 +1,11 @@
+package com.example.universepeace.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by liuyuning on 2017/7/8.
+ */
+
+public class Province extends DataSupport {
+
+}
